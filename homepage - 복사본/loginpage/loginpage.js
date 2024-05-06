79272ac62d111904285a88fdd26fd90ae0f90095
kimchiMan0316 <167506431+kimchiMan0316@users.noreply.github.com>
2024-05-07 03:26:06 +0900

    2번쨰
    확인요망

---
+++ homepage - 복사본/loginpage/loginpage.js
@@ -0,0 +1,9 @@
+function loginAction(){
+    const a = document.getElementById("username");
+    const b = document.getElementById("passwold");
+
+    // 임의의 값을 넣어둔 것임, 서버의 값을 변수로 선언해서 변경할 예정
+    if(a==ppsalho&&b==pp0316){
+        window.location.href = "https://www.naver.com";
+    }
+}
